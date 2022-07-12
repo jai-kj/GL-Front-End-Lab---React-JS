f3d71dc97cd4b963a56918835a20f9671d64168d
jai-kj <jai.j@somaiya.edu>
2022-07-12 10:34:15 +0530

    basic foundation for backend done

--- server/server.js
+++ server/server.js
@@ -1,14 +1,10 @@
 const jsonServer = require("json-server")
-const auth = require("json-server-auth")
 
 const app = jsonServer.create()
 const router = jsonServer.router("db.json")
 
 // Bind router db to app
 app.db = router.db
-
-// Add auth middleware
-app.use(auth)
 
 // Add route middleware
 app.use(router)
